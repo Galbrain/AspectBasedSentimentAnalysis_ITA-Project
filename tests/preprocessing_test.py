d8f7adc7cc6edb2b27b215b02541eb99677c36d9
Daholli <christoph.hollizeck@web.de>
2020-12-20 21:09:44 +0100

    fix: linting

--- tests/preprocessing_test.py
+++ tests/preprocessing_test.py
@@ -1,12 +1,30 @@
+# -*- coding: utf-8 -*-
+import json
 import os
 import unittest
+
+import pandas as pd
 
 from src.utils.preprocessing import Preprocessor
 
 
 class PreprocessingTest(unittest.TestCase):
     def setUp(self):
-        self.preprocessor = Preprocessor("./src/data/raw/")
+        self.path = "./src/data/"
+        self.preprocessor = Preprocessor(self.path)
+        self.test_json = {
+            "reviews": [
+                {
+                    "text": "Das ist für eine Testdatei!",
+                    "rating": {
+                        "Grafik": 4,
+                        "Sound": 5,
+                        "Steuerung": 5,
+                        "Atmosphäre": 5,
+                    },
+                }
+            ]
+        }
 
     def testValidPath(self):
         self.assertTrue(os.path.isdir(self.preprocessor.path))
@@ -14,6 +32,23 @@ class PreprocessingTest(unittest.TestCase):
     def testNoJsonfiles(self):
         tmpprep = Preprocessor("./")
         self.assertRaises(Exception, lambda: tmpprep.find_jsons())
+
+    def testPrepDefault(self):
+        with open(self.path + "testdata.json", "w") as f:
+            json.dump(self.test_json, f, indent=4)
+
+        tmpprep = Preprocessor(self.path)
+        print(tmpprep.prep().tolist())
+        self.assertTrue([["testdatei"]] == tmpprep.prep().tolist())
+
+    def testPrepNoStopwordRemoval(self):
+        with open(self.path + "testdata.json", "w") as f:
+            json.dump(self.test_json, f, indent=4)
+
+        tmpprep = Preprocessor(self.path, rmstopwords=False)
+        self.assertTrue(
+            [["das", "ist", "fuer", "eine", "testdatei"]] == tmpprep.prep().tolist()
+        )
 
 
 if __name__ == "__main__":
