e78d0bf8bbae47550a859a342270a8e28f46151e
Daholli <christoph.hollizeck@web.de>
2021-02-04 12:24:04 +0100

    docs: added documentation

--- src/utils/aspect_annotator.py
+++ src/utils/aspect_annotator.py
@@ -26,7 +26,13 @@ class AspectAnnotator:
             columns=["reviewnumber", "word_found", "word_idx", "aspect"]
         )
 
-    def loadCSV(self, filename: str = "data_preprocessed.csv"):
+    def loadCSV(self, filename: str = "data_preprocessed.csv") -> None:
+        """
+        load CSV from the given filename
+
+        Args:
+            filename (str, optional): String of path to the preprocessed data. Defaults to "data_preprocessed.csv".
+        """
         self.data = pd.read_csv(self.path + filename)
         self.data["tokens"] = (
             self.data["tokens"]
@@ -36,7 +42,13 @@ class AspectAnnotator:
             .values.tolist()
         )
 
-    def findAspects(self, rowDf):
+    def findAspects(self, rowDf: pd.Series) -> None:
+        """
+        function to be vectorized for the dataset
+
+        Args:
+            rowDf (pd.Series): Row of a dataframe containing "index and tokens"
+        """
         aspects = {}
         for aspect in self.keyWords:
             compare = self.keyWords[aspect]
@@ -50,8 +62,17 @@ class AspectAnnotator:
                         aspects["aspect"] = aspect
                         self.df = self.df.append(aspects, ignore_index=True)
 
-    def annotate(self):
+    def annotate(self) -> None:
+        """
+        function to call the "findAspects()" function for every row
+        """
         self.data.apply(lambda x: self.findAspects(x), axis=1)
 
-    def saveCSV(self, filename: str = "data_aspects_tokens.csv"):
+    def saveCSV(self, filename: str = "data_aspects_tokens.csv") -> None:
+        """
+        Save csv that contains the data for the annotation
+
+        Args:
+            filename (str, optional): path to file. Defaults to "data_aspects_tokens.csv".
+        """
         self.df.to_csv(self.path + filename, index=False)
