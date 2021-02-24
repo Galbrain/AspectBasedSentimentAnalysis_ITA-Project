26a80a12cee6c7d34b46b76a79b7c4ebd4521e1e
Jessica <jessica-loehr@web.de>
2021-02-24 19:13:07 +0100

    feat: better sampling

--- src/train.py
+++ src/train.py
@@ -16,21 +16,29 @@ y = data['true_label'].astype(int)
 prep_x = list()
 prep_y = list()
 
+x_train = list()
+x_test = list()
+y_test = list()
+y_train = list()
+
 for elem_x, elem_y in zip(x, y):
     elem_x = elem_x.replace(']', '')
     elem_x = elem_x.replace('[', '')
     try:
         elem_x = float(elem_x)
-        if prep_y.count(elem_y) < 100:
-            prep_x.append([elem_x])
-            prep_y.append(elem_y)
+        if y_train.count(mapping[elem_y]) < 400:
+            x_train.append([elem_x])
+            y_train.append(mapping[elem_y])
+        elif y_test.count(mapping[elem_y]) < 100:
+            x_test.append([elem_x])
+            y_test.append(mapping[elem_y])
     except:
         pass
 
 x_train, x_test, y_train, y_test = train_test_split(
     prep_x, prep_y, test_size=0.25, random_state=42)
 
-print('used ', str((len(prep_x)/len(x))*100) + '%', 'of all sentences')
+print('used ', str(((len(x_train)+len(x_test))/len(x))*100) + '%', 'of all sentences')
 
 naive_bayes = GaussianNB()
 naive_bayes.fit(x_train, y_train)
@@ -43,7 +51,7 @@ print(accuracy)
 fig, ax = plt.subplots()
 cm = confusion_matrix(y_test, y_pred)
 conf = confusion_matrix(y_test, y_pred).ravel()
-cm = conf.reshape(6, 6)
+cm = conf.reshape(3, 3)
 sns.heatmap(cm, annot=True, fmt="d", cmap="Spectral")
 ax.set_xlabel('predicted label')
 ax.set_ylabel('true label')
@@ -51,10 +59,11 @@ fig.savefig('test_nb')
 
 y_pred = lg.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
 fig, ax = plt.subplots()
 cm = confusion_matrix(y_test, y_pred)
 conf = confusion_matrix(y_test, y_pred).ravel()
-cm = conf.reshape(6, 6)
+cm = conf.reshape(3, 3)
 sns.heatmap(cm, annot=True, fmt="d", cmap="Spectral")
 ax.set_xlabel('predicted label')
 ax.set_ylabel('true label')
